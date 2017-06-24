2ec5981d367ff8a692052ed5fc843663d5f6a901
Михаил <mcups@yandex.ru>
2017-06-24 13:05:14 +0300

    Java 2 Homework 5

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,7 @@ public class Main {
 
         System.out.println(System.currentTimeMillis() - a);
 
+        //Проверка содержимого массива
         for (int i = 0; i < 5; i++) {
             System.out.println(arr[i]);
         }
@@ -65,6 +66,7 @@ public class Main {
 
         System.out.println(System.currentTimeMillis() - a);
 
+        //Проверка содержимого массива
         for (int i = 0; i < 5; i++) {
             System.out.println(arr[i]);
         }
